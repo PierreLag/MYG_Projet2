c10b4fc84173bb1f82c50075a12ae62c1c0ce0d0
Pierre Lagoutte <lagoutte_pierre@orange.fr>
2022-11-02 20:18:17 +0100

    Modifications 2/11/22

---
+++ MYG_Projet2/PenduController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MYG_Projet2
+{
+    // Controlleur pour la classe Pendu, permettant au jeu de fonctionner.
+    internal class PenduController
+    {
+        // Liste de mots pouvant être séléctionnés au hasard lors de la construction de l'objet.
+        readonly private static String[] motsListe =
+        {
+            "TEST",
+            "VOCABULAIRE"
+        };
+
+        // Mot à deviner dans le cadre du jeu du pendu
+        readonly private String mot;
+
+
+        private List<Char> characteresUtilises;
+
+        //
+        public PenduController()
+        {
+            Random random = new Random();
+            mot = motsListe[random.Next(motsListe.Length)]; // Sélectionne un mot au hasard dans la liste.
+            characteresUtilises = new List<Char>();
+        }
+
+        public String GetMot() { return mot; }
+
+        public bool IsCharInMot(Char charactere)
+        {
+            return mot.Contains(charactere);
+        }
+    }
+}
